26a50225d02d1c464dc3b7a0b6ac2898aba70839
Briana Muham <61461042+Muhambriana@users.noreply.github.com>
2021-06-17 00:31:45 +0700

    Add Certificate Pinning

--- core/src/main/java/com/muhamapps/filmcatalogueapp1/core/di/CoreModule.kt
+++ core/src/main/java/com/muhamapps/filmcatalogueapp1/core/di/CoreModule.kt
@@ -11,6 +11,7 @@ import com.muhamapps.filmcatalogueapp1.core.utils.AppExecutors
 import com.muhamapps.filmcatalogueapp1.core.utils.NetworkInfo.BASE_URL
 import net.sqlcipher.database.SQLiteDatabase
 import net.sqlcipher.database.SupportFactory
+import okhttp3.CertificatePinner
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.android.ext.koin.androidContext
@@ -32,10 +33,17 @@ val databaseModule = module {
 
 val networkModule = module {
     single {
+        val hostname = "api.themoviedb.org"
+        val certificatePinner = CertificatePinner.Builder()
+            .add(hostname, "sha256/+vqZVAzTqUP8BGkfl88yU7SQ3C8J2uNEa55B7RZjEg0=")
+            .add(hostname, "sha256/JSMzqOOrtyOT1kmau6zKhgT676hGgczD5VMdRMyJZFA=")
+            .add(hostname, "sha256/JSMzqOOrtyOT1kmau6zKhgT676hGgczD5VMdRMyJZFA=")
+            .build()
         OkHttpClient.Builder()
             .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
             .connectTimeout(120, TimeUnit.SECONDS)
             .readTimeout(120, TimeUnit.SECONDS)
+            .certificatePinner(certificatePinner)
             .build()
     }
     single {
